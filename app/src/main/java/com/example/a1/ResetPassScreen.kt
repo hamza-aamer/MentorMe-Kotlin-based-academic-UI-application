3c91fb820dee62542aff261248898f8fabf24266
HamzaVictus <hamzaaamer6@gmail.com>
2024-02-09 03:28:36 +0500

    ResetPassScreen Updated

--- app/src/main/java/com/example/a1/ResetPassScreen.kt
+++ app/src/main/java/com/example/a1/ResetPassScreen.kt
@@ -3,15 +3,14 @@ package com.example.a1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import com.example.a1.databinding.ActivityForgotPassScreenBinding
+import com.example.a1.databinding.ActivityResetPassScreenBinding
 
-class ForgotPassScreen : AppCompatActivity() {
-
-    private lateinit var binding: ActivityForgotPassScreenBinding
+class ResetPassScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityResetPassScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityForgotPassScreenBinding.inflate(layoutInflater)
+        binding = ActivityResetPassScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
@@ -28,10 +27,5 @@ class ForgotPassScreen : AppCompatActivity() {
             finish()
         }
 
-        binding.ForgotPassBtn.setOnClickListener {
-            val intent = Intent(this, ResetPassScreen::class.java)
-            startActivity(intent)
-            finish()
-        }
     }
 }
