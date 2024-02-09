a100ea0c6e36e98a3b6a82ab14b50f490acf0163
HamzaVictus <hamzaaamer6@gmail.com>
2024-02-09 19:25:20 +0500

    BottomViewBar|AppBar Updated

---
+++ app/src/main/java/com/example/a1/BottomNavigationBar.kt
@@ -0,0 +1,43 @@
+package com.example.a1
+
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a1.databinding.ActivityBottomNavigationBarBinding
+
+class BottomNavigationBar : AppCompatActivity() {
+    private lateinit var binding : ActivityBottomNavigationBarBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBottomNavigationBarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replacefrag(HomeFragment())
+        var ret: Boolean = true
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            ret=true
+            when(it.itemId){
+
+                R.id.home-> replacefrag(HomeFragment())
+                R.id.search-> replacefrag(SearchFragment())
+                R.id.chat-> replacefrag(ChatFragment())
+                R.id.profile-> replacefrag(ProfileFragment())
+                R.id.placeholder->  ret=false
+
+                else->{
+
+                }
+
+            }
+            ret
+        }
+
+    }
+
+    private fun replacefrag(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.FrameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
+}
--- app/src/main/java/com/example/a1/LoginScreen.kt
+++ app/src/main/java/com/example/a1/LoginScreen.kt
@@ -6,22 +6,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.example.a1.databinding.ActivityLoginScreenBinding
 
 class LoginScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_screen)
+        binding= ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val Register: TextView = findViewById(R.id.RegisterRedirect)
-        Register.setOnClickListener {
+        binding.RegisterRedirect.setOnClickListener {
             val intent = Intent(this, RegisterScreen::class.java)
             startActivity(intent)
             finish()
         }
 
-        val ForgotPass: TextView = findViewById(R.id.forgotPassword)
-        ForgotPass.setOnClickListener {
+        binding.forgotPassword.setOnClickListener {
             val intent = Intent(this, ForgotPassScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.loginbtn.setOnClickListener{
+            val intent = Intent(this,BottomNavigationBar::class.java)
             startActivity(intent)
             finish()
         }
