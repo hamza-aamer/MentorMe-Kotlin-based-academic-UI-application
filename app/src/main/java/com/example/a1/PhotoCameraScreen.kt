bbd2695e1dbb6a61759e991dda01aa84f3d8d568
HamzaVictus <hamzaaamer6@gmail.com>
2024-02-11 20:32:54 +0500

    PhotoCameraScreen VideoCameraScreen Updated

---
+++ app/src/main/java/com/example/a1/PhotoCameraScreen.kt
@@ -0,0 +1,11 @@
+package com.example.a1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoCameraScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_camera_screen)
+    }
+}
