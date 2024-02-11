183d2ec452150b864272e046c2fbb907f38e4ecc
HamzaVictus <hamzaaamer6@gmail.com>
2024-02-11 23:44:19 +0500

    SplashScreen Updated

--- app/src/main/java/com/example/a1/LoginScreen.kt
+++ app/src/main/java/com/example/a1/LoginScreen.kt
@@ -1,14 +1,13 @@
 package com.example.a1
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.a1.databinding.ActivityLoginScreenBinding
 
 class LoginScreen : AppCompatActivity() {
+
+
     private lateinit var binding: ActivityLoginScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/a1/SplashScreen.kt
+++ app/src/main/java/com/example/a1/SplashScreen.kt
@@ -1,13 +1,14 @@
 package com.example.a1
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.WindowManager
 import android.content.Intent
+import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
